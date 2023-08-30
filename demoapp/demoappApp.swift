d435de33a7e77275cc300c39439f9055a4f5b639
KamsQue <kamqasim1@gmail.com>
2023-08-30 15:17:26 +0200

    Initial Commit

---
+++ demoapp/demoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoappApp.swift
+//  demoapp
+//
+//  Created by KamsQue on 30/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct demoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
